f1646f7cb3107b374a02625bafc2f954f0e2e5e4
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-04-05 08:21:17 -0400

    Updated from Brython Server: 4/5/2018 8:21:16 AM

--- TicTacToe.py
+++ TicTacToe.py
@@ -2,3 +2,33 @@
 #4/4/18
 #TicTacToe.py
 
+from ggame import *
+
+white=Color(0xFFFFFF,1)
+
+blackOutline=LineStyle(1, black)
+
+whiteRectangle=RectangleAsset(150,150,blackOutline,white) 
+whiteRectangle2=RectangleAsset(150,150,blackOutline,white) 
+whiteRectangle3=RectangleAsset(150,150,blackOutline,white) 
+whiteRectangle4=RectangleAsset(150,150,blackOutline,white) 
+whiteRectangle5=RectangleAsset(150,150,blackOutline,white) 
+whiteRectangle6=RectangleAsset(150,150,blackOutline,white) 
+whiteRectangle7=RectangleAsset(150,150,blackOutline,white) 
+whiteRectangle8=RectangleAsset(150,150,blackOutline,white) 
+whiteRectangle9=RectangleAsset(150,150,blackOutline,white) 
+
+
+Sprite(whiteRectangle,(0,150))
+Sprite(whiteRectangle2)
+Sprite(whiteRectangle3),(0,150))
+Sprite(whiteRectangle4)
+Sprite(whiteRectangle5)
+Sprite(whiteRectangle6)
+Sprite(whiteRectangle7)
+Sprite(whiteRectangle8)
+Sprite(whiteRectangle9)
+
+
+App().run()
+
