6345dcfab1c3649c8523c3c4c3f34c0d1165b950
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-04-10 08:47:23 -0400

    Updated from Brython Server: 4/10/2018 8:47:22 AM

--- TicTacToe.py
+++ TicTacToe.py
@@ -11,25 +11,94 @@ def mouseClick(event):
 
     if event.x<=150 and event.y<=150:
         Sprite(X,(0,0))
+        data['square'] = X
+        Computerturn()
+
     elif event.x<=300 and event.y<=150:
         Sprite(X,(150,0))
+        data['square2'] = X
+        Computerturn()
+
     elif event.x<=450 and event.y<=150:
         Sprite(X,(300,0))
+        data['square3'] = X
+        Computerturn()
+
     elif event.x<=150 and event.y<=300:
         Sprite(X,(0,150))
+        data['square4'] = X
+        Computerturn()
+
     elif event.x<=300 and event.y<=300:
         Sprite(X,(150,150))
+        data['square5'] = X
+        Computerturn()
+
     elif event.x<=450 and event.y<=300:
         Sprite(X,(300,150))
+        data['square6'] = X
+        Computerturn()
+
     elif event.x<=150 and event.y<=450:
         Sprite(X,(0,300))
+        data['square7'] = X
+        Computerturn()
+
     elif event.x<=300 and event.y<=450:
         Sprite(X,(150,300))
+        data['square8'] = X
+        Computerturn()
+
     else:
         Sprite(X,(300,300))
+        data['square9'] = X
+        Computerturn()
+
 '''
 def isEmpty ():
     '''
+def Computerturn():
+    from random import randint
+    comp_num=randint(1,9)
+    O=TextAsset('O',fill=black,style='bold 120pt Times')#
+
+    
+    if comp_num==1:
+        Sprite('O',(0,0))
+        data['square'] = O
+
+    elif comp_num==2:
+        Sprite('O',(150,0))
+        data['square'] = O
+
+    elif comp_num==3:
+        Sprite('O',(300,0))
+        data['square'] = O
+
+    elif comp_num==4:
+        Sprite('O',(0,150))
+        data['square'] = O
+
+    elif comp_num==5:
+        Sprite('O',(150,150))
+        data['square'] = O
+
+    elif comp_num==6:
+        Sprite('O',(300,150))
+        data['square'] = O
+
+    elif comp_num==7:
+        Sprite('O',(0,300))
+        data['square'] =O
+
+    elif comp_num==8:
+        Sprite('O',(150,300))
+        data['square'] = O
+
+    elif comp_num==9:
+        Sprite('O',(300,300))
+        data['square'] = O
+
 
 
 if __name__ == '__main__':
