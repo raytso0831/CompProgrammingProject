809d626880719d312a7770fca15452c26d3549dc
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-05-23 08:40:02 -0400

    Update BattleShip.py

--- BattleShip.py
+++ BattleShip.py
@@ -2,4 +2,32 @@
 #5/23/18
 #battleship.py
 
+from ggame import *
+ #graphics
+white=Color(0xFFFFFF,1)
+black=Color(0x000000,1)
+    
 
+blackOutline=LineStyle(1, black)
+whiteRectangle=RectangleAsset(75,75,blackOutline,white) 
+whiteRectangle2=RectangleAsset(75,75,blackOutline,white) 
+whiteRectangle3=RectangleAsset(75,75,blackOutline,white) 
+whiteRectangle4=RectangleAsset(75,75,blackOutline,white) 
+whiteRectangle5=RectangleAsset(75,75,blackOutline,white) 
+whiteRectangle6=RectangleAsset(75,75,blackOutline,white) 
+whiteRectangle7=RectangleAsset(75,75,blackOutline,white) 
+whiteRectangle8=RectangleAsset(75,75,blackOutline,white) 
+whiteRectangle9=RectangleAsset(75,75,blackOutline,white) 
+    
+Sprite(whiteRectangle,(0,0))
+Sprite(whiteRectangle2,(75,0))
+Sprite(whiteRectangle3,(150,0))
+Sprite(whiteRectangle4,(225,0))
+Sprite(whiteRectangle5,(300,0))
+Sprite(whiteRectangle6,(300,150))
+Sprite(whiteRectangle7,(0,300))
+Sprite(whiteRectangle8,(150,300))
+Sprite(whiteRectangle9,(300,300))
+    
+    
+App().run()
