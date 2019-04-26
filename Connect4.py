43e123e711d437cedc778e2dc8942bcb4905a771
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2019-04-26 10:45:46 -0400

    Updated from Brython Server: 4/26/2019 10:45:46 AM

--- Connect4.py
+++ Connect4.py
@@ -9,16 +9,18 @@ def redrawall():
 
     RADIUS=33
     red=Color(0xFF0000,1)
+    white=Color(0xFFFFFF,1)
     black=Color(0x000000,1)
     yellow=Color(0xFFF033,1)
     blue= Color(0x3633FF,1)
     
     blackOutline=LineStyle(1, black)
-    blueRectangle=RectangleAsset(75,75,blackOutline,blue)
+    blueOutline=LineStyle(1, blue)
+    blueRectangle=RectangleAsset(75,75,blueOutline,white)
     redCircle=CircleAsset(35,blackOutline,red)
     yellowCircle=CircleAsset(35,blackOutline,yellow)
-    for r in range(0,7):
-            for c in range(0,6):
+    for r in range(0,6):
+            for c in range(0,7):
                 Sprite(blueRectangle,((2*RADIUS+10)*c,(2*RADIUS+10)*r))
 
 
