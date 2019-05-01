01ad288341ed387fab816446e58981c7f73b6bd7
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2019-05-01 10:08:41 -0400

    Updated from Brython Server: 5/1/2019 10:08:43 AM

--- Connect4.py
+++ Connect4.py
@@ -3,6 +3,7 @@
 #Connect4.py
 
 from random import randint
+from time import time
 from ggame import *
 
 RADIUS=33
@@ -16,22 +17,34 @@ blueOutline=LineStyle(1, blue)
 blueRectangle=RectangleAsset(75,75,blueOutline,white)
 redCircle=CircleAsset(35,blackOutline,red)
 yellowCircle=CircleAsset(35,blackOutline,yellow)
-    
+
+
 def redrawall():
-    for r in range(0,6):
-            for c in range(0,7):
-                Sprite(blueRectangle,((2*RADIUS+10)*c,(2*RADIUS+10)*r))
-                if data[r][c]=='player':
-                    Sprite(redCircle,((2*RADIUS+10)*c,(2*RADIUS+10)*r))
-                elif data[r][c]=='computer':
-                    Sprite(yellowCircle,((2*RADIUS+10)*c,(2*RADIUS+10)*r))
+    for x in range(0,6):
+            for y in range(0,7):
+                Sprite(blueRectangle,((2*RADIUS+10)*y,(2*RADIUS+10)*x))
+                if data['board'][x][y]=='player':
+                    Sprite(redCircle,((2*RADIUS+10)*y,(2*RADIUS+10)*x))
+                elif data['board'][x][y]=='computer':
+                    Sprite(yellowCircle,((2*RADIUS+10)*y,(2*RADIUS+10)*x))
 
 def mouseClick(event):
+    data['click'] = True
+    data['clicktime'] = time()
     if data['Game Over']==False:
-        print(event.x//75,event.y//75)
+        print(event.x//75)
+       
+        if data['board'][event.y//75]!= 'player':
+            data['tokens_placed']+=1
+            data[event.x//75]= 'player'
+    redrawall()
 
-    
-    
+def step():
+    delay = 4
+    if data['click'] and time() > data['clicktime'] + delay:
+        print("CLICK")
+        data['click'] = False
+
     
 def buildBoard():
     board=[['','','','','','player','computer'],['','','','','','',''],['','','','','','',''],['','','','','','',''],['','','','','','',''],['','','','','','','']]
@@ -40,9 +53,11 @@ def buildBoard():
 
 if __name__ == '__main__':
     data={}
-    data=buildBoard()
+    data['board']=buildBoard()
+    data['click']=False
     redrawall()
-    App().run()
+    data['tokens_placed']=0
+    App().run(step)
     App().listenMouseEvent('click',mouseClick)
     data['Game Over']=False
 
