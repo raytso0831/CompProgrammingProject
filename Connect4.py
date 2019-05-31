5e9ea5528929314f581855d40349e1107532aa3a
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2019-05-31 11:59:44 -0400

    Updated from Brython Server: 5/31/2019 11:59:43 AM

--- Connect4.py
+++ Connect4.py
@@ -2,6 +2,7 @@
 #4/26/19
 #Connect4.py
 
+#These are imports used.
 from random import randint
 from time import time
 from ggame import *
@@ -18,7 +19,7 @@ blueRectangle=RectangleAsset(75,75,blueOutline,white)
 redCircle=CircleAsset(35,blackOutline,red)
 yellowCircle=CircleAsset(35,blackOutline,yellow)
 
-
+# This function will draws the board
 def redrawall():
     for x in range(0,6):
         for y in range(0,7):
@@ -29,7 +30,8 @@ def redrawall():
                 Sprite(yellowCircle,((2*RADIUS+10)*y,(2*RADIUS+10)*x))
     check_row_for_winner()
 
-
+#This function should figure out what column the user clicked on. It also makes sure that the computer is allowed 
+# to make a move if you clicked outside the board
 def mouseClick(event):
     if event.x>= (75*7) or event.y >= (75*6):
         return
@@ -48,7 +50,8 @@ def mouseClick(event):
     winner()
     redrawall()
 
-
+# This function will select a random number and then places it on a board without being allowed to place two tokens on top of each other
+#It also makes sures that if a computer is about to win it will make the winning move
 def computer_put_token():
     data['click'] = True
     data['clicktime'] = time()
@@ -76,14 +79,16 @@ def computer_put_token():
     #winner()
     redrawall()
     
-    
+#This functions delays the computer to make its turn  1.5 second to make seem smarter   
 def step():
     delay = 1.5
     if data['click'] and time() > data['clicktime'] + delay:
         computer_put_token()
         data['click'] = False
-        
-def check_row_for_winner():
+
+
+#This function check the row of the board to see if there is a winner    
+        def check_row_for_winner():
     for y in range(6):
         currplayer = None
         count = 0
@@ -97,6 +102,7 @@ def check_row_for_winner():
                 currplayer = data['board'][y][x]
     return ''
 
+#This function check the column of the board to see if there is a winner
 def check_col_for_winner():
     for y in range(7):
         currplayer = None
@@ -111,7 +117,7 @@ def check_col_for_winner():
                 currplayer = data['board'][x][y]
     return ''
     
-    
+#This function check the left side diagonally of the board to see if there is a winner    
 def diagonal_left():
     startposition=[(0,3),(0,4),(0,5),(1,5),(2,5),(3,5)]
     for x, y in startposition:
@@ -128,6 +134,7 @@ def diagonal_left():
             y-=1
 
 
+#This function check the right side diagonally of the board to see if there is a winner    
 
 def diagonal_right():
     startposition=[(0,0),(0,1),(0,2),(1,0),(2,0),(3,0)]
@@ -144,6 +151,7 @@ def diagonal_right():
             x+=1
             y+=1
 
+#This function return True if the user or the computer won and False otherwise.
 def winner():
     computer_winner=TextAsset('Computer Wins!!! ',fill=black,style='bold 40pt Arial')
     user_winner=TextAsset('YOU WIN!!!!!',fill=black,style='bold 40pt Arial')
@@ -157,12 +165,13 @@ def winner():
         return True
     else:
         return False
-        
+
+#Creates a 7x6 empty matrix and returns it.
 def buildBoard():
     board=[['','','','','','',''],['','','','','','',''],['','','','','','',''],['','','','','','',''],['','','','','','',''],['','','','','','','']]
     return board
 
-
+#This will store all of the code and will setup the game
 if __name__ == '__main__':
     data={}
     data['board']=buildBoard()
